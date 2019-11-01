d0b15675b12522cf9ddf35e7a3a63b4bb169a068
AlejandroCader <miguel.ccader@gmail.com>
2019-11-01 11:28:47 -0600

    Updates

---
+++ precache-manifest.1ded62f620a81e16bb3ef22dfb305fdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be6bc471fa89654f4020e532aa0fce5",
+    "url": "/TICTACTOE/index.html"
+  },
+  {
+    "revision": "f4ab5ac49dcf906f698e",
+    "url": "/TICTACTOE/static/css/main.29dbe5b5.chunk.css"
+  },
+  {
+    "revision": "5fc33aed0e7becc2a37c",
+    "url": "/TICTACTOE/static/js/2.52dd166e.chunk.js"
+  },
+  {
+    "revision": "f4ab5ac49dcf906f698e",
+    "url": "/TICTACTOE/static/js/main.95565eae.chunk.js"
+  },
+  {
+    "revision": "b95b34d6bd69652c6841",
+    "url": "/TICTACTOE/static/js/runtime-main.ba3c8e1c.js"
+  }
+]);
